c231002f2fbf4d8f2b329b452fcdabeec0b01a57
fokhrul2029 <ifokhrul.2029@gmail.com>
2022-10-12 16:57:27 +0600

    first commit

--- js/script.js
+++ js/script.js
@@ -1,6 +1,41 @@
+// Animation On Scroll------------
 AOS.init();
-
+// Preloader---------
 const loader = document.querySelector(".loader")
 window.onload = () => {
     loader.classList.add("disppear")
 }
+// Navigation Js-----------
+// When the user scrolls down 80px from the top of the document, resize the navbar's padding and the logo's font size
+window.onscroll = () => {
+    if (document.body.scrollTop>80 || document.documentElement.scrollTop>80 ) {
+        // document.querySelector("#navbar").style.padding="0px 0px ";
+        document.querySelector("#logo").style.fontSize = "20px "; 
+    }else{
+        // document.querySelector("#navbar").style.padding="20px 0px ";
+        document.querySelector("#logo").style.fontSize = "35px "; 
+    }
+}
+
+// var triggerEl = document.querySelector('#myTab a[href="#home"]')
+// bootstrap.Tab.getInstance(triggerEl).show() // Select tab by name
+
+// var triggerFirstTabEl = document.querySelector('#myTab li:first-child a')
+// bootstrap.Tab.getInstance(triggerFirstTabEl).show() // Select first tab
+
+
+var triggerTabList = [].slice.call(document.querySelectorAll('#myTab a'))
+triggerTabList.forEach(function (triggerEl) {
+  var tabTrigger = new bootstrap.Tab(triggerEl)
+
+  triggerEl.addEventListener('click', function (event) {
+    event.preventDefault()
+    tabTrigger.show()
+  })
+})
+
+
+
+
+
+
