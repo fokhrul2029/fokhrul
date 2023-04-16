d37d3cc633a31c09bd2c96cb62159242df6c71c3
fokhrul2029 <ifokhrul.2029@gmail.com>
2023-04-16 20:07:44 +0600

    first commit

--- js/script.js
+++ js/script.js
@@ -1,27 +1,24 @@
 // Animation On Scroll------------
 AOS.init();
 // Preloader---------
-const loader = document.querySelector(".loader")
+const loader = document.querySelector(".loader");
 window.onload = () => {
-    loader.classList.add("disppear")
-}
+  loader.classList.add("disppear");
+};
 // Navigation Js-----------
 // When the user scrolls down 80px from the top of the document, resize the navbar's padding and the logo's font size
 window.onscroll = () => {
-    if (document.body.scrollTop>80 || document.documentElement.scrollTop>80 ) {
-        // document.querySelector("#navbar").style.padding="0px 0px ";
-        document.querySelector("#logo").style.fontSize = "20px "; 
-    }else{
-        // document.querySelector("#navbar").style.padding="20px 0px ";
-        document.querySelector("#logo").style.fontSize = "35px "; 
-    }
-}
-var triggerTabList = [].slice.call(document.querySelectorAll('#myTab a'))
+  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+    document.querySelector("#logo").style.fontSize = "20px ";
+  } else {
+    document.querySelector("#logo").style.fontSize = "35px ";
+  }
+};
+var triggerTabList = [].slice.call(document.querySelectorAll("#myTab a"));
 triggerTabList.forEach(function (triggerEl) {
-  var tabTrigger = new bootstrap.Tab(triggerEl)
+  var tabTrigger = new bootstrap.Tab(triggerEl);
 
-  triggerEl.addEventListener('click', function (event) {
-    // event.preventDefault();
-    tabTrigger.show()
-  })
-})
+  triggerEl.addEventListener("click", function (event) {
+    tabTrigger.show();
+  });
+});
